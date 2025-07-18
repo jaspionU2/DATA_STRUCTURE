2c4f16cfe79e4b778698dbef3c4a1d651d025c86
jaspionU2 <138932022+jaspionU2@users.noreply.github.com>
2025-07-18 20:07:28 +0000

    function InsertInTree is working correctly. The function SearchInList has been added as well

--- BINARY-TREE/binary-tree.c
+++ BINARY-TREE/binary-tree.c
@@ -9,6 +9,22 @@ typedef struct BinaryTree{
 
 BinaryTree* root = NULL;
 
+BinaryTree* CreateNode(int data);
+void InsertInTree(int data, BinaryTree ** current_node);
+BinaryTree* SearchInList(int data_searched, BinaryTree ** current_node);
+
+int main(){
+    InsertInTree(20, &root);
+    InsertInTree(10, &root);
+    InsertInTree(5, &root);
+    InsertInTree(30, &root);
+    InsertInTree(30, &root);
+    SearchInList(5, &root);
+    
+
+    return 0;
+}
+
 BinaryTree* CreateNode(int data){
     BinaryTree * Node = (BinaryTree*) malloc(sizeof(BinaryTree));
 
@@ -23,26 +39,36 @@ BinaryTree* CreateNode(int data){
     return Node;
 };
 
-BinaryTree* InsertInTree(int data, BinaryTree ** current_node){ 
+void InsertInTree(int data, BinaryTree ** current_node){ 
     if (*current_node == NULL){
         *current_node = CreateNode(data);
         return;
     }
-
-    if (data < (*current_node)->data){
+    
+      if (data < (*current_node)->data){
         if ((*current_node)->left_node == NULL){
             (*current_node)->left_node = CreateNode(data);
-            return *current_node;
+            return;
         }
-        (*current_node)->left_node = InsertInTree(data, &((*current_node)->left_node));
-        return *current_node;
-    } else if (data > (*current_node)->data) {
-         if ((*current_node)->right_node == NULL){
+        InsertInTree(data, &((*current_node)->left_node));
+      } else {
+        if ((*current_node)->right_node == NULL){
             (*current_node)->right_node = CreateNode(data);
-            return *current_node;
+            return;
         }
-        (*current_node)->right_node = InsertInTree(data, &((*current_node)->right_node));
+        InsertInTree(data, &((*current_node)->right_node));
+      }
+   
+};
+
+BinaryTree* SearchInList(int data_searched, BinaryTree ** current_node){
+    if (*current_node == NULL || (*current_node)->data == data_searched){
         return *current_node;
     }
-    
+
+    if (data_searched < (*current_node)->data){
+        SearchInList(data_searched, &((*current_node)->left_node));
+    } else {
+        SearchInList(data_searched, &((*current_node)->right_node));
+    }
 }
