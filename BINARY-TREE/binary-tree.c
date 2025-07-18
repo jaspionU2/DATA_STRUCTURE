693db0876133f39e99fc32c3e41bd053119a3af4
jaspionU2 <138932022+jaspionU2@users.noreply.github.com>
2025-07-18 02:33:27 +0000

    added a function to insert a node in the binary tree

--- BINARY-TREE/binary-tree.c
+++ BINARY-TREE/binary-tree.c
@@ -7,6 +7,7 @@ typedef struct BinaryTree{
     struct BinaryTree * right_node;
 } BinaryTree;
 
+BinaryTree* root = NULL;
 
 BinaryTree* CreateNode(int data){
     BinaryTree * Node = (BinaryTree*) malloc(sizeof(BinaryTree));
@@ -20,4 +21,25 @@ BinaryTree* CreateNode(int data){
     Node->left_node = Node->right_node = NULL;
 
     return Node;
+};
+
+BinaryTree* InsertInTree(int data, BinaryTree ** current_node){ 
+    BinaryTree * node = CreateNode(data);
+
+    if (root == NULL){
+        root = node;
+        return;
+    }
+
+    if (data < (*current_node)->data && (*current_node)->left_node == NULL){
+        (*current_node)->left_node = node;
+        return current_node;
+    }
+    if (data > (*current_node)->data && (*current_node)->right_node == NULL){
+       (*current_node)->right_node = node;
+       return current_node;
+    }
+
+    InsertInTree(data, current_node);
+    return;
 }
